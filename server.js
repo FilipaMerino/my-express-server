2e6c8f25de7f115b1a85a65c9d6f1bb94891f695
Filipa Merino <filipaam@outlook.com>
2023-03-14 12:23:36 +0000

    Server using express.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// jshint esversion:6
+const express = require("express");
+
+const app = express();  // Function that represents the Express module, and we bind that to the word app
+
+app.listen(3000); // this tells it to listen on a specific port
