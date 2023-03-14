022c6e18a1da2beab0d424feece91f7f8be0012e
Filipa Merino <filipaam@outlook.com>
2023-03-14 12:36:24 +0000

    understanding server request and responses

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const express = require("express");
 
 const app = express();  // Function that represents the Express module, and we bind that to the word app
 
-app.listen(3000); // this tells it to listen on a specific port
+// We need to specify what should happen when a browser gets in touch with our server and makes a get request
+
+app.get("/", function(req, res){
+  res.send("<h1>Hello World</h1>");
+}); // req === request; res === response;
+
+app.listen(3000, function(){
+  console.log("Server started running on port 3000");
+}); // this tells it to listen on a specific port
