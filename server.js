6a2b550af9d7c13527d112227baf963427f7737c
Filipa Merino <filipaam@outlook.com>
2023-03-14 12:46:52 +0000

    Creating some servers

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const app = express();  // Function that represents the Express module, and we b
 app.get("/", function(req, res){
   res.send("<h1>Hello World</h1>");
 }); // req === request; res === response;
+// this part: app.get("/") => specifies the route that we are going to respond to.
+// We can respond to different routes;
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at: filipaam@outlook.com");
+});
+
+
+//Exercise: Create a new route, so when you go to the About page of the website at local host, you are able to see a quick brief bio of who you are
+
+app.get("/about", function(req, res){
+  res.send("Hi everyone 👋🏼 <br>I’m @FilipaMerino and I used to be a neuropsychologist 🧠 Crazy, right? <br>I started learning code in my spare time, and I found it so enjoyable that I enrolled at Le Wagon Web Development Bootcamp to become a Full Stack or Front-end Developer 👩🏻‍💻.<br><br>I'm currently learning React 🎨");
+});
+
+
+app.get("/blog", function(req, res){
+  res.send("<h1>THIS IS MY BLOG</h1>");
+});
 
 app.listen(3000, function(){
   console.log("Server started running on port 3000");
